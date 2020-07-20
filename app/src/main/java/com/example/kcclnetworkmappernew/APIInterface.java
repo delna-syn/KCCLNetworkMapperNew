fed89d1ef8db8b4d635dd3da7e7786bf9399f9b4
delna <delna@syntrio.in>
2020-07-20 14:16:15 +0530

    Initial commit

---
+++ app/src/main/java/com/example/kcclnetworkmappernew/APIInterface.java
@@ -0,0 +1,10 @@
+package com.example.kcclnetworkmappernew;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface APIInterface {
+
+//    @GET("/api/users?page=2")
+//    Call<EmployeeDetails> doGetListResources();
+}
